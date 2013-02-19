977b4e9e77313c2c08a229679ae51c00c060f72f
Mou <mahmoud@mail.usf.edu>
2013-02-19 18:49:17 -0500

    fixed sort.py

--- sorting.py
+++ sorting.py
@@ -2,16 +2,18 @@
 
 import string 
 
-list_a = []
+list_a = []#the list that will contain the string
 
 def sorting(imput):
-   list_a = append(imput.split(' '))
-   list_a.sort()
+  'this will take a string split it into a list then sort it'
+  list_a.append(imput.split())
+  return list_a.sort()
    
    
 def main():
+   'this is main'
    imput = raw_input('>Enter list separated by SPACE: ')
-   sorting(imput)
+   imput = sorting(imput)
    print imput
    
 if __name__ == '__main__':
