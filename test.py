2f4339bf8a0c743da02657d6f814b1237ae69ef9
loca1h0st <mahmoud@mail.usf.edu>
2013-02-19 19:52:58 -0500

    Update test.py
    Cleaned it up

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 def trial_division(n):
-    """Return a list of the prime factors for a natural number."""
+    'Return a list of the prime factors for a natural number.'
     if n == 1: return [1]
     primes = prime_sieve(int(n**0.5) + 1)
     prime_factors = []
@@ -11,16 +11,13 @@ def trial_division(n):
             n //= p
     if n > 1: prime_factors.append(n)
  
-    return prime_factors
-    
+    return prime_factors  
     
 def main():
+ 'this is main'
  n = int(raw_input('> '))
- 
  prime = trial_division(n)
- 
  print n
- 
  
 if __name__ == '__main__':
   main()
